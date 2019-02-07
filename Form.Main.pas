6edb7823922cef4360dc45a8fdb80ff594e9e1e4
Bogdan Polak <bogdan.polak@bsc.com.pl>
2019-02-07 13:44:58 +0100

    Dodano przykład na OnFormReady i OnFormIdle

--- Form.Main.pas
+++ Form.Main.pas
@@ -3,11 +3,15 @@ unit Form.Main;
 interface
 
 uses
-  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Winapi.Windows, Winapi.Messages,
+  System.SysUtils, System.Variants, System.Classes,
+  Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
 type
   TForm1 = class(TForm)
+    GroupBox1: TGroupBox;
+    Button1: TButton;
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +24,41 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  // x
+end;
+
+// -------------------------------------------------------------------
+// -------------------------------------------------------------------
+// Sample:
+// -------------------------------------------------------------------
+(*
+  OnFormReady := procedure(Sender: TObject)
+    begin
+      with TButton.Create(Self) do
+      begin
+        AlignWithMargins := True;
+        Align := alTop;
+        Caption := 'Button Jeden';
+        Parent := GroupBox1;
+      end;
+    end;
+  OnFormIdle := procedure(Sender: TObject)
+    var
+      btn: TButton;
+    begin
+      if GroupBox1.ControlCount > 0 then
+      begin
+        btn := (GroupBox1.Controls[0] as TButton);
+        btn.Tag := btn.Tag + 1;
+        btn.Caption := btn.Tag.ToString;
+      end;
+    end;
+*)
+// -------------------------------------------------------------------
+// -------------------------------------------------------------------
+
 
 end.
