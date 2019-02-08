7f5f61fe9f661d34466df56048b4666ff24e0a53
Bogdan Polak <bogdan.polak@bsc.com.pl>
2019-02-08 13:01:49 +0100

    Uporządkowano źródła

--- FormPlusDemo.dpr
+++ FormPlusDemo.dpr
@@ -3,15 +3,13 @@ program FormPlusDemo;
 uses
   Vcl.Forms,
   Form.Main in 'Form.Main.pas' {Form1},
-  Plus.Vcl.Form in 'Plus.Vcl.Form.pas',
-  Plus.Vcl.Action in 'Plus.Vcl.Action.pas',
-  Plus.Vcl.ActionGuiBuilder in 'Plus.Vcl.ActionGuiBuilder.pas',
-  Plus.Vcl.Frame in 'Plus.Vcl.Frame.pas',
   Frame.FirstOne in 'Frame.FirstOne.pas' {Frame1: TFrame},
-  Plus.Vcl.Timer in 'Plus.Vcl.Timer.pas',
-  Plus.Vcl.PageControl in 'Plus.Vcl.PageControl.pas',
-  Frame.WithLabel in 'Frame.WithLabel.pas' {FrameWithLabel: TFrame},
-  Frame.WithStringGrid in 'Frame.WithStringGrid.pas' {FrameWithStringGrid: TFrame};
+  Plus.Vcl.Action in 'plus\Plus.Vcl.Action.pas',
+  Plus.Vcl.ActionGuiBuilder in 'plus\Plus.Vcl.ActionGuiBuilder.pas',
+  Plus.Vcl.Form in 'plus\Plus.Vcl.Form.pas',
+  Plus.Vcl.Frame in 'plus\Plus.Vcl.Frame.pas',
+  Plus.Vcl.PageControl in 'plus\Plus.Vcl.PageControl.pas',
+  Plus.Vcl.Timer in 'plus\Plus.Vcl.Timer.pas';
 
 {$R *.res}
 
